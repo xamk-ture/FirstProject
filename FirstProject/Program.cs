6b9b163c54e07e4af4b4fcd245ff640dceef5ea5
Tuomas Reijonen <hture01@xamk.fi>
2023-09-12 20:12:24 +0300

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace FirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tehtävä 1: Tulosta "Hello World!"
+
+            //Tehtävä 2: Kysy käyttäjältä nimi ja tulosta "Terve, <name>!"
+
+            //Tehtävä 3: Poista nimestä turhat välilyönnit
+
+            //Tehtävä 4: Muuta nimen ensimmäinen kirjain isoksi ja muut pieniksi käyttämällä CapitalizeFirstLetter funktiota
+
+            //Tehtävä 5: Kysy käyttäjältä sukunimi omaan muuttujaan ja tulosta "Terve, <firstName> <lastName>!"
+            //Tulosta käyttäjän nimi kolmella eri tavalla, käyttäen eri string tekniikoita
+            //Katso esimerkkiä SecondProject\Program.cs
+
+            //Tehtävä 6: Kommentoi koodi selventämään, että mitä se tekee
+
+        }
+
+        private static string CapitalizeFirstLetter(string? inputString)
+        {
+            /*inputString.First().ToString().ToUpper(): Ottaa ensimmäisen merkin inputString-merkkijonosta, 
+             * muuttaa sen takaisin merkkijonoksi char tyyppisestä muuttujasta ja muuttaa sen isoksi kirjaimeksi (eli alkukirjaimeksi).
+
+             inputString.Substring(1).ToLower(): Ottaa inputString-merkkijonon loput merkit ensimmäisen merkin jälkeen ja muuttaa ne pieniksi kirjaimiksi.
+             Lopuksi yllä mainitut kaksi osaa yhdistetään yhteen merkkijonoon käyttäen + -operaattoria.
+             Koodin tulos on, että inputString-merkkijonon ensimmäinen kirjain muutetaan isoksi kirjaimeksi ja kaikki muut merkit pieniksi kirjaimiksi. 
+             Esimerkiksi jos inputString oli alun perin "hELLo", sen arvo koodin suorittamisen jälkeen on "Hello".*/
+
+            inputString = inputString.First().ToString().ToUpper() + inputString.Substring(1).ToLower();
+            return inputString;
+        }
+
+
+    }
+}
---
+++ SecondProject/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä x");
+
+            var x = Console.ReadLine();
+
+            Console.WriteLine("Syötä y");
+
+            var y = Console.ReadLine();
+
+            var result = int.Parse(x) + int.Parse(y);
+
+            Console.WriteLine("Combine result + is " + result);
+            Console.WriteLine(string.Format("String format result is {0}", result));
+            Console.WriteLine($"Interpolation result $ is {result}");
+
+
+            Console.WriteLine("Combine example: Result " + x + " +  " + y + " = " + result);
+            Console.WriteLine(string.Format("string.Format example: Result {0} + {1} = {2}", x, y, result));
+            Console.WriteLine($"Interpolation example: Result {x} + {y} = {result}");
+        }
+    }
+}
